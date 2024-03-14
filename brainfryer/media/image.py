7bcd367847275c9e641563a55127841128e90037
Sofiane H. DJERBI <sofiane.djerbi38@gmail.com>
2024-03-14 19:11:26 +0100

    Reduce latency

--- brainfryer/media/image.py
+++ brainfryer/media/image.py
@@ -11,25 +11,26 @@ class ImageAgent:
         self.text_agent = text_agent
     
     def generate(self, message, attempt=1):
-        print(f"Generating \"{message}\"...")
         pattern = re.compile(r'nacked|nude|naked', re.IGNORECASE)
         message = pattern.sub('skin-colored', message).replace("\"", "")
+        print(f"Generating \"{message}\"...")
 
-        start_time = time.time()
         try:
             completion = self.client.images.generate(prompt=message, n=1, size="1024x1024", model=self.model) # 1024*1792
         except BadRequestError as e:
-            print(f"Message denied: \"{message}\"")
-            if attempt > MAX_ATTEMPT:
-                raise e
-            print("Retrying...")
-            new_message = self.text_agent.send_message(
-                f"This text does not pass DALL-E content policy. Rewrite it: {message}"
-            )
-            print(f"New message: \"{new_message}\"")
-            return self.generate(new_message, attempt + 1)
-
-        execution_time = time.time() - start_time
-        time.sleep(max(60 - execution_time, 0))
+            print(e)
+            if (e.code == "content_policy_violation"):
+                print(f"Message denied: \"{message}\"")
+                if attempt > MAX_ATTEMPT:
+                    raise e
+                print("Retrying...")
+                new_message = self.text_agent.send_message(
+                    f"This text does not pass DALL-E content policy. Rewrite it: {message}"
+                )
+                print(f"New message: \"{new_message}\"")
+                return self.generate(new_message, attempt + 1)
+            else:
+                time.sleep(10)
+                return self.generate(new_message, attempt)
 
         return completion.data[0].url
