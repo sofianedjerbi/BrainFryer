550626da5cb693aaa97b190d9d10dfd0db03b0e8
Sofiane H. Djerbi <sofiane.djerbi38@gmail.com>
2024-03-18 02:55:13 +0100

    Add Subtitles (#1)
    * Subtitles (WIP)
    
    * Full engine for subtitles
    
    * Subtitle demo

--- brainfryer/__main__.py
+++ brainfryer/__main__.py
@@ -7,9 +7,9 @@ from brainfryer.creator import *
 LOGO = """
   ____               _         _____                         
  | __ )  _ __  __ _ (_) _ __  |  ___|_ __  _   _   ___  _ __ 
- |  _ \ | '__|/ _` || || '_ \ | |_  | '__|| | | | / _ \| '__|
+ |  _ \\ | '__|/ _` || || '_ \\ | |_  | '__|| | | | / _ \\| '__|
  | |_) || |  | (_| || || | | ||  _| | |   | |_| ||  __/| |   
- |____/ |_|   \__,_||_||_| |_||_|   |_|    \__, | \___||_|   
+ |____/ |_|   \\__,_||_||_| |_||_|   |_|    \\__, | \\___||_|   
                                            |___/             
 """
 
@@ -22,13 +22,15 @@ GPT_KEY = os.getenv('OPENAI_KEY')
 GPT_MODEL_TEXT = os.getenv('OPENAI_MODEL_TEXT')
 GPT_MODEL_IMAGE = os.getenv('OPENAI_MODEL_IMAGE')
 GPT_MODEL_TTS = os.getenv('OPENAI_MODEL_TTS')
+GPT_MODEL_SUBTITLES = os.getenv('OPENAI_MODEL_WHISPER')
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Generate attention-grabbing videos!', prog='python -m brainfryer')
     parser.add_argument('url', help='Reddit URL')
-    parser.add_argument('--background', help='Youtube background URL (optional)')
-    parser.add_argument('--song', help='Youtube song URL (optional)')
-    parser.add_argument('--gen-images', action='store_true', help='Generate images (default: False)')
+    parser.add_argument('-b', '--background', help='Youtube background URL (optional)')
+    parser.add_argument('-s', '--song', help='Youtube song URL (optional)')
+    parser.add_argument('-i', '--gen-images', action='store_true', help='Generate images (default: False)')
+    parser.add_argument('-t', '--gen-subtitles', action='store_true', help='Generate subtitles (default: False)')
 
     args = parser.parse_args()
     return args
@@ -42,6 +44,7 @@ def main():
         background = args.background or DEFAULT_BACKGROUND
         song = args.song or DEFAULT_SONG
         gen_images = args.gen_images
+        gen_subtitles = args.gen_subtitles
     else:
         print(LOGO)
 
@@ -51,10 +54,10 @@ def main():
         song = input("Youtube song url (empty = default): ") or DEFAULT_SONG
         gen_images = True if input("Generate images (Y/N)? ").strip().lower() == 'y' else False
 
-    creator = VideoCreator(GPT_KEY, GPT_MODEL_IMAGE, GPT_MODEL_TEXT, GPT_MODEL_TTS)
+    creator = VideoCreator(GPT_KEY, GPT_MODEL_IMAGE, GPT_MODEL_TEXT, GPT_MODEL_TTS, GPT_MODEL_SUBTITLES)
 
     # Generate
-    creator.generate_from_reddit_comments(url, background, song, gen_images)
+    creator.generate_from_reddit_comments(url, background, song, gen_images, gen_subtitles)
 
 if __name__ == "__main__":
     main()
---
+++ brainfryer/media/subtitles.py
@@ -0,0 +1,59 @@
+import os
+from faster_whisper import WhisperModel
+from moviepy.editor import VideoFileClip, CompositeVideoClip, TextClip, ColorClip
+
+FONT_SIZE = 80
+
+class SubtitleAgent:
+    def __init__(self, output_file, model):
+        self.output_file = output_file
+        self.model = WhisperModel(model)
+    
+    def generate_subtitles(self):
+        segments, _ = self.model.transcribe(self.output_file, word_timestamps=True)
+
+        video = VideoFileClip(self.output_file)
+        height = video.size[1] // 4
+        clips = [video]
+
+        for segment in segments:
+            for word in segment.words:
+                start_time, end_time = word.start, word.end
+                text = word.word.strip()
+
+                txt_clip = (TextClip(
+                                text,
+                                fontsize=FONT_SIZE,
+                                color='white',
+                                font='Arial-Bold'
+                            )
+                            .set_position(('center', 'bottom'))
+                            .margin(bottom=height, opacity=0)
+                            .set_duration(end_time - start_time)
+                            .set_start(start_time))
+                
+                txt_width, txt_height = txt_clip.size
+                
+                bg_clip = (ColorClip(
+                               size=(txt_width + 24, FONT_SIZE + 10),  # Match the size with your text clip or the desired background size
+                               color=(0, 0, 0, 128),  # RGBA (Black with 50% opacity), adjust the alpha value (0-255) for different opacities
+                               duration=txt_clip.duration
+                           )
+                           .set_position(('center', 'bottom'))
+                           .margin(bottom=height, opacity=0)
+                           .set_start(start_time))
+
+                clips.append(bg_clip)
+                clips.append(txt_clip)
+
+        final_clip = CompositeVideoClip(clips)
+
+        dir_name, file_name = os.path.split(self.output_file)
+        final_output = os.path.join(dir_name, "subtitled_" + file_name)
+        final_clip.write_videofile(
+            final_output,
+            codec="libx264", 
+            audio_codec="aac",
+            ffmpeg_params=["-profile:v", "baseline", "-pix_fmt", "yuv420p"],
+            fps=30
+        )
